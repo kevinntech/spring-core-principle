b9059c199f1faeb5954b1ad68fee4916ff654064
kevinntech <kevinntech@nate.com>
2021-03-12 21:50:29 +0900

    [ 스프링 핵심 원리 이해1 - 예제 만들기 ] : 회원 도메인 개발

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,17 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        // 다형성에 의해 MemoryMemberRepository의 save()가 호출된다.
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
